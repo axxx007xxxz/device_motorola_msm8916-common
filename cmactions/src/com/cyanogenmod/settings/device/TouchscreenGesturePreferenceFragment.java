931efd80e6352fe1476e32307602e04b23702833
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-09-07 18:20:51 +0800

    cmactions: Integrate with Settings
    Bring this up to speed on the Settings changes in N
    
    Change-Id: I9365b3ebd1bbfed2936302e30da50e3f9af06665

--- cmactions/src/com/cyanogenmod/settings/device/TouchscreenGesturePreferenceFragment.java
+++ cmactions/src/com/cyanogenmod/settings/device/TouchscreenGesturePreferenceFragment.java
@@ -23,12 +23,10 @@ import android.os.Bundle;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.preference.Preference;
-import android.preference.PreferenceFragment;
-import android.preference.PreferenceCategory;
-import android.preference.SwitchPreference;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v14.preference.PreferenceFragment;
+import android.support.v7.preference.Preference;
+import android.support.v14.preference.SwitchPreference;
 
 public class TouchscreenGesturePreferenceFragment extends PreferenceFragment {
     private static final String CATEGORY_AMBIENT_DISPLAY = "ambient_display_key";
@@ -37,8 +35,7 @@ public class TouchscreenGesturePreferenceFragment extends PreferenceFragment {
     private boolean mFlipClick = false;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.gesture_panel);
         PreferenceCategory ambientDisplayCat = (PreferenceCategory)
                 findPreference(CATEGORY_AMBIENT_DISPLAY);
